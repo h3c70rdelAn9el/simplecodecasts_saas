eb26bcceb9292c1c25438752e59f625a0e2a8bc1
H3C70R <hectordelangel@gmail.com>
2017-05-14 23:27:33 +0000

    trying to fix the stripe integration with customer card attached

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,7 @@ class ContactsController < ApplicationController
     end
     
     def create
-        @contact = Contact.new(contact_params
-        )
+        @contact = Contact.new(contact_params)
         
         if @contact.save
             flash[:success] = 'Message sent.'
