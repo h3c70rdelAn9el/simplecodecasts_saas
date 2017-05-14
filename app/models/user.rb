551b2bc92c8066984563f10642cd502ce25678f3
H3C70R <hectordelangel@gmail.com>
2017-05-14 21:53:58 +0000

    Modified devise for stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
         
   belongs_to :plan 
+  attr_accessor :stipre_card_token
+  
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: srtipe_card_token)
+      self.stripe_customer_token = customer_id
+      save!
+    end
+  end
+  
 end
