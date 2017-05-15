4fc328427b891b274e00f58bd15c1491a5dd64e1
H3C70R <hectordelangel@gmail.com>
2017-05-15 18:35:03 +0000

    fixed the links!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,28 @@
 class ContactsController < ApplicationController
-    def new
-        @contact = Contact.new
-    end
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
     
-    def create
-        @contact = Contact.new(contact_params)
-        
-        if @contact.save
-            flash[:success] = 'Message sent.'
-            redirect_to new_contact_path
-        else
-            flash[:danger] = 'Error occured, message has not been sent.'
-            redirect_to new_contact_path
-        end
+    if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      
+      ContactMailer.contact_email(name, email, body).deliver
+      
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been sent.'
+      redirect_to new_contact_path
     end
-
-
-private
+  end
+  
+  private
     def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
+      params.require(:contact).permit(:name, :email, :comments)
     end
-
-end 
+end
