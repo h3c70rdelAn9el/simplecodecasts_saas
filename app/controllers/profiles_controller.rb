04146505c5a523816c0cd34c642529b214a1fac6
H3C70R <hectordelangel@gmail.com>
2017-05-23 17:01:27 +0000

    Added profiles_new action

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,7 @@
+class ProfilesController < ApplicationController
+    def new
+        # form where a user can fill out their OWN profile. 
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile
+    end
+end
