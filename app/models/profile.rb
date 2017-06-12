afa48264a393589d428722dccc8124de774e3a76
H3C70R <hectordelangel@gmail.com>
2017-06-12 18:22:13 +0000

    improved users index page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
     belongs_to :user
 
     has_attached_file :avatar, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
